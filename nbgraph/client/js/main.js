b6a8a2ab48c5dcdf82691075829c0301801527a8
Paul <paul@Pauls-MacBook-Air.local>
2016-11-06 04:15:54 -0500

    more warning messages

--- nbgraph/client/js/main.js
+++ nbgraph/client/js/main.js
@@ -4,4 +4,10 @@ if (!('sigma' in window)) {
     {% include 'js/sigma.layout.forceAtlas2.min.js' %}
 }
 
+if ('nbgraph' in window) {
+    document.getElementById('nbgraph_confirm_{{container_id}}').innerHTML =
+        'WARNING: nbgraph javascript loaded multiple times. Use ' +
+        '<samp>renderer.prepare_notebook()</samp> ' +
+        'and call <samp>.show()</samp> on graphs to reduce notebook file size.';
+}
 {% include 'js/nbgraph.js' %}
